393ee356194d0020eab3602250739bee1c3c3545
Heather Ho <heatherho@gmail.com>
2025-03-06 21:24:43 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,16 @@ import pandas as pd
 
 st.title('🤖Machine Learning App')
 
-st.write('This app builds a machine learning model!')
+st.info('This app builds a machine learning model!')
+with st.expander('Data'):
+  st.write('**Raw Data**')
+  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/da9653dfdb975b7342fa3a1a3b74375e35a9c1e5/penguins_cleaned.csv')
+  df
 
-df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/da9653dfdb975b7342fa3a1a3b74375e35a9c1e5/penguins_cleaned.csv')
-df
+  st.write('**X**')
+  X = df.drop('species', axis=1)
+  X
+
+st.write('**y**')
+y = df.species
+y
