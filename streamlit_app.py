538bb460b8d11bbff88e266fba3c8cab252f97e8
Heather Ho <heatherho@gmail.com>
2025-03-06 22:50:36 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,7 +51,7 @@ input_row = df_penguins[:1]
 
 # Encode y
 target_mapper = {'Adele': 0,
-                 'Chinstrap: 1,
+                 'Chinstrap': 1,
                  'Gentoo': 2}
 def target_encode(val):
   return target_mapper[val]
