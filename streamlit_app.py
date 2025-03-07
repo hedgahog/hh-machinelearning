dcfc1e540852be88d04263d65af23c78985145cc
Heather Ho <heatherho@gmail.com>
2025-03-07 09:41:39 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@ from sklearn.ensemble import RandomForestClassifier
 st.title('🤖Machine Learning App')
 
 st.info('This app builds a machine learning model!')
+
 with st.expander('Data'): # Dropdown for data below
   st.write('**Raw Data**')
   df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/da9653dfdb975b7342fa3a1a3b74375e35a9c1e5/penguins_cleaned.csv')
@@ -18,6 +19,7 @@ with st.expander('Data'): # Dropdown for data below
   st.write('**y**')
   y_raw = df.species
   y_raw
+  
 # Scatter plot for data
 with st.expander('Data Visualization'):
   st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
@@ -31,6 +33,7 @@ with st.sidebar:
   flipper_length_mm = st.slider('Flipper length (mm)', 172.0, 231.0, 201.0)
   body_mass_g = st.slider('Body mass(g)', 2700.0, 6300.0, 4207.0)
   gender = st.selectbox('Gender', ('Male', 'Female'))
+  
 # create dataframe for input features
   data = {'island': island,
           'bill_length_mm': bill_length_mm,
@@ -40,11 +43,13 @@ with st.sidebar:
           'sex': gender}
   input_df = pd.DataFrame(data, index=[0])
   input_penguins = pd.concat([input_df, X_raw], axis=0)
+  
 with st.expander('Input features'):
   st.write('**Input penguin**')
   input_df
   st.write('**Combined penguin data**')
   input_penguins
+  
 # Data preparation
 # Encode X
 encode = ['island', 'sex']
@@ -59,10 +64,9 @@ target_mapper = {'Adelie': 0,
                  'Gentoo': 2}
 def target_encode(value):
   return target_mapper[value]
+  
 y_raw = y_raw.str.strip().str.capitalize()
 y = y_raw.apply(target_encode)
-y
-y_raw
 
 with st.expander('Data preparation'):
   st.write('**Encoded X (input penguin)**')
