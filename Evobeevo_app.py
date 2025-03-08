d67270e855ecea614f77a802436d68341f564cd9
Heather Ho <heatherho@gmail.com>
2025-03-07 17:15:17 -0800

    Update Evobeevo_app.py

--- Evobeevo_app.py
+++ Evobeevo_app.py
@@ -2,4 +2,9 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title("EvoBees Variant Analysis")
+st.title("EvoBees Variant Predictor 🐝")
+
+# Display result and score on main page
+# Display CliVar comparison on main page as well?
+# Sidebar will have user input info, or should it also be on main page
+# Where to put chatbot?
